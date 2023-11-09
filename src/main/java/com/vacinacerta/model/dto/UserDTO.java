e62617322c12e8a895c981e0c2ec313623af8e04
jayv1s <joaovsgcontato@gmail.com>
2023-11-09 06:33:46 -0300

    feat(model): added user models for view, mapper and dto

--- src/main/java/com/vacinacerta/model/dto/UserDTO.java
+++ src/main/java/com/vacinacerta/model/dto/UserDTO.java
@@ -1,2 +1,22 @@
-package com.vacinacerta.model.dto;public class UserDTO {
+package com.vacinacerta.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+public class UserDTO {
+    public String id;
+    public String firstName;
+    public String lastName;
+    public LocalDateTime dateOfBirth;
+    public String document;
+    public String documentType;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
+    //TODO: adicionar e-mail
+    private Set<Object> vaccines;
 }
